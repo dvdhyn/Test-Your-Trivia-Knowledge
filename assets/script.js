d6d0ba8ac87daae98e175f5af85178b617a41c86
yungtaco <dvhyun@gmail.com>
2023-10-30 13:55:51 -0400

    Added Q&A array

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,25 @@
 var count=10;
 var timeCount=document.querySelector("#timeCount");
 var start=document.querySelector("#startQuiz");
-var question=document.querySelector("#question");
+var questionText=document.querySelector("#question");
 var choice=document.querySelector(".choice");
+const database=[{
+    question:"How tall is Mount Everest?",
+    answers:[
+        {text: "28,760 ft", value:0},
+        {text: "32,490 ft", value:0},
+        {text: "30,610 ft", value:0},
+        {text: "29,032 ft", value:1}
+    ]
+},{
+    question:"Which Greek philosopher was the first to develop a formal system for reasoning?",
+    answers:[
+        {text: "Plato", value:0},
+        {text: "Aristotle", value:1},
+        {text: "Socrates", value:0},
+        {text: "Pythagoras", value:0}
+    ]
+}];
 
 start.addEventListener("click",function(){
     setInterval(function(){
@@ -17,6 +34,6 @@ start.addEventListener("click",function(){
 start.addEventListener("click",function(){
     var begin = document.querySelector("#begin");
     begin.style.display = "none";
-    question.innerText="Question #1";
+    questionText.innerText="Question #1";
     choice.style.display = "block";
 });
