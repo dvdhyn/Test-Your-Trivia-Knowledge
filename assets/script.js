cbdb611242368740d0764a67240580ee70e4e9c5
yungtaco <dvhyun@gmail.com>
2023-10-30 15:57:46 -0400

    Added choice feedback and scorekeeping

--- assets/script.js
+++ assets/script.js
@@ -3,24 +3,27 @@ var timeCount=document.querySelector("#timeCount");
 var start=document.querySelector("#startQuiz");
 var questionText=document.querySelector("#que");
 var choice=document.querySelector(".choice");
-let queCount=0;
+var response=document.querySelector("#response");
 let score=0;
+
 const qaBank=[{
     question:"How tall is Mount Everest?",
     answers:[
-        {text: "28,760 ft", value:0},
-        {text: "32,490 ft", value:0},
-        {text: "30,610 ft", value:0},
-        {text: "29,032 ft", value:1}
-    ]
+        {text: "28,760 ft"},
+        {text: "32,490 ft"},
+        {text: "30,610 ft"},
+        {text: "29,032 ft"}         // correct
+    ],
+    correct:"29,032 ft"
 },{
     question:"Which Greek philosopher was the first to develop a formal system for reasoning?",
     answers:[
-        {text: "Plato", value:0},
-        {text: "Aristotle", value:1},
-        {text: "Socrates", value:0},
-        {text: "Pythagoras", value:0}
-    ]
+        {text: "Plato"},
+        {text: "Aristotle"},         // correct
+        {text: "Socrates"},
+        {text: "Pythagoras"}
+    ],
+    correct:"Aristotle"
 }];
 
 start.addEventListener("click",function(){
@@ -33,13 +36,33 @@ start.addEventListener("click",function(){
     }},1000);
 });
 
-start.addEventListener("click",function(){
+
+function beginQuiz(){
     var begin = document.querySelector("#begin");
     begin.style.display = "none";
     choice.style.display = "block";
     let queT=qaBank[0].question;
-    let answerT='<div class="answ">'+qaBank[0].answers[0].text+'</div>'+'<div class="answ">'+qaBank[0].answers[1].text+'</div>'+'<div class="answ">'+qaBank[0].answers[2].text+'</div>'+'<div class="answ">'+qaBank[0].answers[3].text+'</div>';
+    let answerT='<div class="answ" id="a1">'+qaBank[0].answers[0].text+'</div>'+'<div class="answ" id="a2">'+qaBank[0].answers[1].text+'</div>'+'<div class="answ" id="a3">'+qaBank[0].answers[2].text+'</div>'+'<div class="answ" id="a4">'+qaBank[0].answers[3].text+'</div>';
     questionText.innerHTML=queT;
     choice.innerHTML=answerT;
-});
+    var answ=choice.querySelectorAll(".answ");
+    for (let i = 0; i < answ.length; i++) {
+        answ[i].setAttribute("onclick", "answSelected(this)");
+    }
+};
 
+function answSelected(select){
+    let userChoice = select.textContent;
+    let correctChoice = qaBank[0].correct;
+    if (userChoice == correctChoice) {
+        score += 1;
+        response.textContent="Correct!" + "Your score is: " + score;
+        console.log(score);
+    } else {
+        score -= 1;
+        response.textContent="Inorrect!"  + "Your score is: " + score;
+        console.log(score);
+    }
+}
+
+start.addEventListener("click",beginQuiz);
