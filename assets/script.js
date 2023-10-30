6b211f2b0d58bdb9a531ecb4b3b89df07b96c080
yungtaco <dvhyun@gmail.com>
2023-10-30 13:44:59 -0400

    Added question elements and script

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var count=10;
 var timeCount=document.querySelector("#timeCount");
 var start=document.querySelector("#startQuiz");
+var question=document.querySelector("#question");
+var choice=document.querySelector(".choice");
 
 start.addEventListener("click",function(){
     setInterval(function(){
@@ -10,4 +12,11 @@ start.addEventListener("click",function(){
         } else {
             clearInterval(count);
     }},1000);
+});
+
+start.addEventListener("click",function(){
+    var begin = document.querySelector("#begin");
+    begin.style.display = "none";
+    question.innerText="Question #1";
+    choice.style.display = "block";
 });
