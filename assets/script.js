2a457994b60bc4a9938a6b01ab1a784081ffc106
yungtaco <dvhyun@gmail.com>
2023-10-30 16:18:11 -0400

    Tested second question functionality

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
-var count=10;
+var count=30;
 var timeCount=document.querySelector("#timeCount");
 var start=document.querySelector("#startQuiz");
 var questionText=document.querySelector("#que");
 var choice=document.querySelector(".choice");
 var response=document.querySelector("#response");
 let score=0;
+let questNo=1;
 
-const qaBank=[{
+let qaBank=[{
     question:"How tall is Mount Everest?",
     answers:[
         {text: "28,760 ft"},
@@ -41,10 +42,10 @@ function beginQuiz(){
     var begin = document.querySelector("#begin");
     begin.style.display = "none";
     choice.style.display = "block";
-    let queT=qaBank[0].question;
-    let answerT='<div class="answ" id="a1">'+qaBank[0].answers[0].text+'</div>'+'<div class="answ" id="a2">'+qaBank[0].answers[1].text+'</div>'+'<div class="answ" id="a3">'+qaBank[0].answers[2].text+'</div>'+'<div class="answ" id="a4">'+qaBank[0].answers[3].text+'</div>';
-    questionText.innerHTML=queT;
-    choice.innerHTML=answerT;
+    let queOne=qaBank[questNo].question;
+    let answerOne='<div class="answ" id="a1">'+qaBank[questNo].answers[0].text+'</div>'+'<div class="answ" id="a2">'+qaBank[questNo].answers[1].text+'</div>'+'<div class="answ" id="a3">'+qaBank[questNo].answers[2].text+'</div>'+'<div class="answ" id="a4">'+qaBank[questNo].answers[3].text+'</div>';
+    questionText.innerHTML=queOne;
+    choice.innerHTML=answerOne;
     var answ=choice.querySelectorAll(".answ");
     for (let i = 0; i < answ.length; i++) {
         answ[i].setAttribute("onclick", "answSelected(this)");
@@ -53,7 +54,7 @@ function beginQuiz(){
 
 function answSelected(select){
     let userChoice = select.textContent;
-    let correctChoice = qaBank[0].correct;
+    let correctChoice = qaBank[questNo].correct;
     if (userChoice == correctChoice) {
         score += 1;
         response.textContent="Correct!" + "Your score is: " + score;
